e673c95cf31d700b22fec18df34b5d2cba044d73
ayyymiel <gozonamiel@gmail.com>
2022-11-04 21:00:10 -0400

    smol edit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import './App.css';
+import SidebarMenu from 'react-bootstrap-sidebar-menu';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
-    <div>Hello</div>
-  )
+    <Container>
+      <Row style={{"padding-top": "100px"}}>
+        <Col>
+          <SidebarMenu>
+            <SidebarMenu.Body>
+              <SidebarMenu.Nav>
+                <SidebarMenu.Nav.Link href="https://www.linkedin.com/in/amielgozon/">
+                  LinkedIn
+                </SidebarMenu.Nav.Link>
+              </SidebarMenu.Nav>
+              <SidebarMenu.Nav>
+                <SidebarMenu.Nav.Link href="https://github.com/ayyymiel">
+                  GitHub
+                </SidebarMenu.Nav.Link>
+              </SidebarMenu.Nav>
+            </SidebarMenu.Body>
+          </SidebarMenu>
+        </Col>
+        <Col>
+          <h1>Amiel Dominic Gozon</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default App;
