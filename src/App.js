ea1752c5f82ea40f22b10fb6527f003974ad4f4f
ayyymiel <gozonamiel@gmail.com>
2022-11-05 18:34:35 -0400

    added bootstrap linking

--- src/App.js
+++ src/App.js
@@ -6,29 +6,31 @@ import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
-    <Container>
-      <Row style={{"padding-top": "100px"}}>
-        <Col>
-          <SidebarMenu>
-            <SidebarMenu.Body>
-              <SidebarMenu.Nav>
-                <SidebarMenu.Nav.Link href="https://www.linkedin.com/in/amielgozon/">
-                  LinkedIn
-                </SidebarMenu.Nav.Link>
-              </SidebarMenu.Nav>
-              <SidebarMenu.Nav>
-                <SidebarMenu.Nav.Link href="https://github.com/ayyymiel">
-                  GitHub
-                </SidebarMenu.Nav.Link>
-              </SidebarMenu.Nav>
-            </SidebarMenu.Body>
-          </SidebarMenu>
-        </Col>
-        <Col>
-          <h1>Amiel Dominic Gozon</h1>
-        </Col>
-      </Row>
-    </Container>
+    <div>
+      <Container>
+        <Row style={{"padding-top": "100px"}}>
+          <Col>
+            <SidebarMenu>
+              <SidebarMenu.Body>
+                <SidebarMenu.Nav>
+                  <SidebarMenu.Nav.Link href="https://www.linkedin.com/in/amielgozon/">
+                    LinkedIn
+                  </SidebarMenu.Nav.Link>
+                </SidebarMenu.Nav>
+                <SidebarMenu.Nav>
+                  <SidebarMenu.Nav.Link href="https://github.com/ayyymiel">
+                    GitHub
+                  </SidebarMenu.Nav.Link>
+                </SidebarMenu.Nav>
+              </SidebarMenu.Body>
+            </SidebarMenu>
+          </Col>
+          <Col >
+            <h1>Amiel Dominic Gozon</h1>
+          </Col>
+        </Row>
+      </Container>
+  </div>
   );
 }
 
