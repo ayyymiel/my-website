d58e4259ea97a39ba1e0e15572ce3592450045a5
ayyymiel <gozonamiel@gmail.com>
2023-01-06 13:44:26 -0500

    added main pic and name

--- src/App.js
+++ src/App.js
@@ -1,36 +1,43 @@
 import './App.css';
-import SidebarMenu from 'react-bootstrap-sidebar-menu';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Table from 'react-bootstrap/Table';
+import Image from 'react-bootstrap/Image';
 
 function App() {
   return (
     <div>
       <Container>
-        <Row style={{"padding-top": "100px"}}>
+        <Row>
+          <Col style={{"padding-top": "50px"}}>
+            <Row>
+              <Table>
+                <thead>
+                  <th width="10%">About</th>
+                  <th width="10%">Experience</th>
+                  <th width="10%">Contact</th>
+                </thead>
+              </Table>
+            </Row>
+            <div style={{"padding-top": "20px"}}>
+              <h1>Amiel</h1>
+              <h1>Dominic</h1>
+              <h1>Gozon</h1>
+            </div>
+          </Col>
           <Col>
-            <SidebarMenu>
-              <SidebarMenu.Body>
-                <SidebarMenu.Nav>
-                  <SidebarMenu.Nav.Link href="https://www.linkedin.com/in/amielgozon/">
-                    LinkedIn
-                  </SidebarMenu.Nav.Link>
-                </SidebarMenu.Nav>
-                <SidebarMenu.Nav>
-                  <SidebarMenu.Nav.Link href="https://github.com/ayyymiel">
-                    GitHub
-                  </SidebarMenu.Nav.Link>
-                </SidebarMenu.Nav>
-              </SidebarMenu.Body>
-            </SidebarMenu>
+            <Image 
+              src={require("./images/310283892_408614708131926_2498100502036794162_n.jpg")}
+              alt="me"
+              style={{"position": "absolute", "top": 0, "right": 0, "object-fit":"cover"}}
+              height="600px"
+              width="600px"
+              />
           </Col>
-          <Col >
-            <h1>Amiel Dominic Gozon</h1>
-          </Col>
-        </Row>
+        </Row> 
       </Container>
-  </div>
+    </div>
   );
 }
 
